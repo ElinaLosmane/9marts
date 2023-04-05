69185fff7eb2c0e7ccd397f46d1068081590230c
Elina <elina.losmane@grobinasskola.lv>
2023-04-05 19:56:59 +0300

    Update

--- kalk.py
+++ kalk.py
@@ -10,7 +10,7 @@ mansLogs.geometry("300x300")
 
 
 #jādefinē pogas
-e=Entry(mansLogs, width=15, font=(""))
+e=Entry(mansLogs, width=15, font=("Arial"))
 
 btn1=Button(mansLogs, text='1', padx='40', pady='20')
 btn2=Button(mansLogs, text='2', padx='40', pady='20')
@@ -25,8 +25,8 @@ btn8=Button(mansLogs, text='8', padx='40', pady='20')
 btn9=Button(mansLogs, text='9', padx='40', pady='20')
 
 btn0=Button(mansLogs, text='0', padx='40', pady='20')
-btn0=Button(mansLogs, text='0', padx='40', pady='20')
-btn0=Button(mansLogs, text='0', padx='40', pady='20')
+plus=Button(mansLogs, text='+', padx='40', pady='20')
+minus=Button(mansLogs, text='-', padx='40', pady='20')
 
 #katra poga jānoliek logā
 
@@ -44,7 +44,7 @@ btn9.grid(row=1, column=2)
 
 btn0.grid(row=4, column=0)
 plus.grid(row=4, column=1)
-minus.grid(row-4, column=2)
+minus.grid(row=4, column=2)
 
 
 mansLogs.mainloop()
