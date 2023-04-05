bac86e97ca8712b4213f55c11c0b3e3fff6e0b4a
Elina <elina.losmane@grobinasskola.lv>
2023-04-05 20:26:20 +0300

    Update

--- kalk.py
+++ kalk.py
@@ -10,25 +10,33 @@ mansLogs.geometry("300x300")
 
 
 #jādefinē pogas
-e=Entry(mansLogs, width=15, font=("Arial"))
+e=Entry(mansLogs, width=30, font=("Arial"))
 
-btn1=Button(mansLogs, text='1', padx='40', pady='20')
-btn2=Button(mansLogs, text='2', padx='40', pady='20')
-btn3=Button(mansLogs, text='3', padx='40', pady='20')
+btn1=Button(mansLogs, text='1', padx='30', pady='20')
+btn2=Button(mansLogs, text='2', padx='30', pady='20')
+btn3=Button(mansLogs, text='3', padx='30', pady='20')
 
-btn4=Button(mansLogs, text='4', padx='40', pady='20')
-btn5=Button(mansLogs, text='5', padx='40', pady='20')
-btn6=Button(mansLogs, text='6', padx='40', pady='20')
+btn4=Button(mansLogs, text='4', padx='30', pady='20')
+btn5=Button(mansLogs, text='5', padx='30', pady='20')
+btn6=Button(mansLogs, text='6', padx='30', pady='20')
 
-btn7=Button(mansLogs, text='7', padx='40', pady='20')
-btn8=Button(mansLogs, text='8', padx='40', pady='20')
-btn9=Button(mansLogs, text='9', padx='40', pady='20')
+btn7=Button(mansLogs, text='7', padx='30', pady='20')
+btn8=Button(mansLogs, text='8', padx='30', pady='20')
+btn9=Button(mansLogs, text='9', padx='30', pady='20')
 
-btn0=Button(mansLogs, text='0', padx='40', pady='20')
-plus=Button(mansLogs, text='+', padx='40', pady='20')
-minus=Button(mansLogs, text='-', padx='40', pady='20')
+btn0=Button(mansLogs, text='0', padx='30', pady='20')
+plus=Button(mansLogs, text='+', padx='30', pady='20')
+minus=Button(mansLogs, text='-', padx='30', pady='20')
+
+rezultats=Button(mansLogs, text='=', padx='20', pady='120')
+
+logs=Entry(mansLogs, text='0')
 
 #katra poga jānoliek logā
+
+logs.grid(row=0, columnspan=4)
+
+rezultats.grid(rowspan=4, column=4)
 
 btn1.grid(row=3, column=0)
 btn2.grid(row=3, column=1)
